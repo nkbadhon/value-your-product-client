ff333b6364e0fb7c9e6c07d3e9fb06d480b61e8f
nkbadhon <badhon7574@gmail.com>
2022-11-29 20:42:53 +0600

    format

--- src/Pages/SubCatagories/SubCatagoriy.js
+++ src/Pages/SubCatagories/SubCatagoriy.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SubCatagoriy = ({ subCategory, setModalDetails }) => {
     console.log(subCategory);
-    const { Product_Name, img, name, Location, Original_Price, Resell_Price, Years_of_Use, Category_id } = subCategory;
+    const { Product_Name, img, name, Location, Original_Price, Resell_Price, Years_of_Use } = subCategory;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl ">
             <figure><img src={img} alt="Shoes" /></figure>
@@ -11,6 +11,7 @@ const SubCatagoriy = ({ subCategory, setModalDetails }) => {
 
             <div className="card-body">
                 <h2 className="card-title font-bold text-info text-center">{Product_Name}</h2>
+                <p className='text-lime-800'>Mobile Name: {name}</p>
                 <p className='text-lime-800'>Seller Name: {name}</p>
                 <p className='text-lime-800'>Location: {Location}</p>
                 <p className='text-lime-800'>Original Price: {Original_Price}</p>
