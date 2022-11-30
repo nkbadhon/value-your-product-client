ede4ab49e0e0b61a23ed36997751bc14c15f2cb7
nkbadhon <badhon7574@gmail.com>
2022-11-30 16:47:15 +0600

    Done

--- src/Pages/AllAvailableProducts/Singlefromall.js
+++ src/Pages/AllAvailableProducts/Singlefromall.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Singlefromall = ({ single, setModalDetails }) => {
 
-    const { img, Brand_name, name, Location, Original_Price, Resell_Price, Years_of_Use, } = single;
+    const { img, name, Location, Original_Price, Resell_Price, Years_of_Use, Product_Name } = single;
 
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl ">
             <figure><img src={img} alt="Shoes" /></figure>
             <div className="card-body">
-                <h2 className="card-title">{Brand_name}</h2>
+                <h2 className="card-title">{Product_Name}</h2>
                 <p>Seller:{name}</p>
                 <p>Location: {Location}</p>
                 <p>Original Price: {Original_Price}</p>
